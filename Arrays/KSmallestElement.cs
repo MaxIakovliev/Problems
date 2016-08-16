2178fbf71e93c59637573384929f18f7dc4b1b09
Max <max.iakovliev@gmail.com>
2016-08-16 21:48:37 +0800

    KSmallestElement

---
+++ Arrays/KSmallestElement.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.Arrays
+{
+    /// <summary>
+    /// http://www.geeksforgeeks.org/kth-smallestlargest-element-unsorted-array/
+    /// K’th Smallest/Largest Element in Unsorted Array
+    /// </summary>
+    class KSmallestElement
+    {
+        public int SortSolution(int[]a, int k)
+        {
+            Array.Sort(a);
+            return a[k];
+        }
+
+
+    }
+}
