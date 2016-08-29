f5c5e70999e80ea40e0eab993addbbc35e66edd1
Max <max.iakovliev@gmail.com>
2016-08-29 18:07:20 +0800

    ZigZagConversion solution accepted by leetcode

--- Leetcode/ZigZagConversion.cs
+++ Leetcode/ZigZagConversion.cs
@@ -6,11 +6,15 @@ using System.Threading.Tasks;
 
 namespace Problems.Leetcode
 {
+    /// <summary>
+    /// https://leetcode.com/problems/zigzag-conversion/
+    /// </summary>
     public class ZigZagConversion
     {
-        public string Solution1(string s, int nRows)
+        public string Solution1(string s, int nRows, out List<char[]> result)
         {
-            var result = new List<char[]>();
+            result = new List<char[]>();
+            if (nRows == 1) return s;
             int i = 0;
             int offset = 0;
             bool isSolid = true;
@@ -24,32 +28,31 @@ namespace Problems.Leetcode
                     {
                         arr[j] = s[i + j];
                         j++;
-                        offset++;
                     }
-                    i += j;
+                    offset = nRows - 1;
+                    i = i + j;
                     isSolid = false;
                 }
                 else
                 {
-                    i++;
                     if (i >= s.Length)
                      break;
 
-                    arr[--offset] = s[i];
+                    arr[--offset] = s[i++];
                     
                 }
                 result.Add(arr);
 
-                if (offset == 0)
+                if (offset == 1)
                     isSolid = true;
             }
             var sb = new StringBuilder();
-            for(i=0; i<result.Count; i++)
-                for(int j=0; j<nRows; j++)
+            for(i=0; i<nRows; i++)
+                for (int j = 0; j < result.Count; j++)
                 {
-                    if(result[i][j]!=default(char))
+                    if(result[j][i]!=default(char))
                     {
-                        sb.Append(result[i][j]);
+                        sb.Append(result[j][i]);
                     }
                 }
             return sb.ToString();
