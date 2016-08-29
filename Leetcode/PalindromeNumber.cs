b5955eca3ecb5c8447320827aab22ad9d56b496a
Max <max.iakovliev@gmail.com>
2016-08-29 22:05:15 +0800

    PalindromeNumber accepted by leetcode

---
+++ Leetcode/PalindromeNumber.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.Leetcode
+{
+    public class PalindromeNumber
+    {
+        public bool IsPalindrome(int x)
+        {
+            var num = x;
+            if (x < 10 && x >= 0) return true;
+            int result = 0;
+            bool negative = x < 0 ? true : false;
+            while (x > 0)
+            {
+                if (negative)
+                    result = (result * 10) - (x % 10);
+                else
+                    result = (result * 10) + (x % 10);
+                x /= 10;
+            }
+            return result == num;
+        }
+    }
+}
