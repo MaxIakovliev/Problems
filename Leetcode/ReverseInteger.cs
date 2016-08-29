1a5ffe1996dc70d6a541a0845e178df542a462af
Max <max.iakovliev@gmail.com>
2016-08-29 18:41:05 +0800

    ReverseInteger accepted by leetcode

---
+++ Leetcode/ReverseInteger.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.Leetcode
+{
+    public class ReverseInteger
+    {
+        public int Reverse(int num)
+        {
+            if (num < 10 && num >=0) return num;
+            var negative = num < 0;
+            if (num < 0) num *= -1;
+            var result = 0;
+            try
+            {
+                checked
+                {
+                    while (num > 0)
+                    {
+                        result = result * 10 + num % 10;
+                        num /= 10;
+                    }
+                }
+            }
+            catch
+            {
+                return 0;
+            }
+            return negative ? result * -1 : result;
+        }
+    }
+}
--- Leetcode/ZigZagConversion.cs
+++ Leetcode/ZigZagConversion.cs
@@ -6,6 +6,17 @@ using System.Threading.Tasks;
 
 namespace Problems.Leetcode
 {
+/* Description of Zig Zag pattern
+ * n=numRows
+ * Δ=2n-2    1                           2n-1                         4n-3
+ * Δ=        2                     2n-2  2n                    4n-4   4n-2
+ * Δ=        3               2n-3        2n+1              4n-5       .
+ * Δ=        .           .               .               .            .
+ * Δ=        .       n+2                 .           3n               .
+ * Δ=        n-1 n+1                     3n-3    3n-1                 5n-5
+ * Δ=2n-2    n                           3n-2                         5n-4
+*/
+
     /// <summary>
     /// https://leetcode.com/problems/zigzag-conversion/
     /// </summary>
