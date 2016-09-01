0cf25b43b21597cc08d03e9702ff61a69256d0e1
Max <max.iakovliev@gmail.com>
2016-09-01 23:31:33 +0800

    updated ValidParentheses

---
+++ Leetcode/ValidParentheses.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.Leetcode
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ValidParentheses
+    {
+        public bool Solution1(string s)
+        {
+            if (s.Length % 2 != 0) return false;
+            var close = new Stack<char>();
+            char c1 = ')', c2 = '}', c3 = ']';
+            for (int i = 0; i < s.Length; i++)
+            {
+                switch (s[i])
+                {
+                    case '(':
+                        close.Push(c1);
+                        break;
+                    case '{':
+                        close.Push(c2);
+                        break;
+                    case '[':
+                        close.Push(c3);
+                        break;
+
+                    case ')':
+
+                    case ']':
+
+                    case '}':
+                        if (close.Count == 0 || close.Pop() != s[i])
+                            return false;
+                        break;
+                    default: break;
+
+                }
+            }
+            return close.Count == 0 ? true : false;
+        }
+    }
+}
