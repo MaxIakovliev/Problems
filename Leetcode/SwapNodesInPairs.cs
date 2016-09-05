472b7f000d06141f70edb445036c41339d91c014
Max <max.iakovliev@gmail.com>
2016-09-05 23:00:51 +0800

    SwapNodesInPairs initial commit

---
+++ Leetcode/SwapNodesInPairs.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.Leetcode
+{
+    public class SwapNodesInPairs
+    {
+        public ListNode SwapPairs(ListNode head)
+        {
+
+            var tmp = head;
+            //var s = new ListNode[3];
+            //ListNode a = null, b = null, c = null, 
+            ListNode prev = null;
+            bool firstStep = true;
+            while (true)
+            {
+                if (tmp == null)
+                    break;
+
+                if (tmp.next == null)
+                {
+                    break;
+                }
+
+                if (tmp.next.next == null)
+                {
+                    var a1 = tmp;
+                    var b1 = tmp.next;
+                    b1.next = a1;
+
+                    if (firstStep)
+                    {
+                        head = b1;
+                        firstStep = false;
+                        prev = a1;
+                    }
+                    else
+                    {
+                        prev.next = b1;
+                        prev = prev.next;
+                        prev = prev.next;
+                    }
+                    break;
+                }
+
+                var a = tmp;
+                var b = tmp.next;
+                var c = tmp.next.next;
+
+
+                a.next = c;
+                b.next = a;
+
+                if (firstStep)
+                {
+                    head = b;
+                    firstStep = false;
+                    prev = a;
+                }
+                else
+                {
+                    prev.next = b;
+                    prev = prev.next;
+                    prev = prev.next;
+
+                }
+
+                tmp = c;
+
+
+            }
+            return head;
+        }
+    }
+}
---
+++ ProblemTest/Leetcode/SwapNodesInPairsTest.cs
@@ -0,0 +1,110 @@
+﻿using NUnit.Framework;
+using Problems.Leetcode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemTest.Leetcode
+{
+    [TestFixture]
+    public class SwapNodesInPairsTest
+    {
+        [Test]
+        public void TestSwapNodes1()
+        {
+            var input = new int[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+            var output = new int[] { 2, 1, 4, 3, 6, 5, 8, 7 };
+            var p = new SwapNodesInPairs();
+
+            ListNode data = null;
+            ListNode tmp = null;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (data == null)
+                {
+                    data = new ListNode(input[i]);
+                    tmp = data;
+                }
+                else
+                {
+                    tmp.next = new ListNode(input[i]);
+                    tmp = tmp.next;
+                }
+            }
+            var result = p.SwapPairs(data);
+            for (int i = 0; i < input.Length; i++)
+            {
+                Console.WriteLine("expected:{0}, actual{1}", output[i], result.val);
+                Assert.AreEqual(output[i], result.val);
+                result = result.next;
+            }
+        }
+
+        [Test]
+        public void TestSwapNodes2()
+        {
+            var input = new int[] { 1, 2, 3, 4, 5, 6, 7, };
+            var output = new int[] { 2, 1, 4, 3, 6, 5, 7 };
+            var p = new SwapNodesInPairs();
+
+            ListNode data = null;
+            ListNode tmp = null;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (data == null)
+                {
+                    data = new ListNode(input[i]);
+                    tmp = data;
+                }
+                else
+                {
+                    tmp.next = new ListNode(input[i]);
+                    tmp = tmp.next;
+                }
+            }
+            var result = p.SwapPairs(data);
+            for (int i = 0; i < input.Length; i++)
+            {
+                Console.WriteLine("expected:{0}, actual{1}", output[i], result.val);
+                Assert.AreEqual(output[i], result.val);
+                result = result.next;
+            }
+        }
+
+        [Test]
+        public void TestSwapNodes3()
+        {
+            var input = new int[] { 1, 2, 3, };
+            var output = new int[] { 2, 1, 3};
+            var p = new SwapNodesInPairs();
+
+            ListNode data = null;
+            ListNode tmp = null;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (data == null)
+                {
+                    data = new ListNode(input[i]);
+                    tmp = data;
+                }
+                else
+                {
+                    tmp.next = new ListNode(input[i]);
+                    tmp = tmp.next;
+                }
+            }
+            var result = p.SwapPairs(data);
+            for (int i = 0; i < input.Length; i++)
+            {
+                Console.WriteLine("expected:{0}, actual{1}", output[i], result.val);
+                Assert.AreEqual(output[i], result.val);
+                result = result.next;
+            }
+        }
+    }
+}
