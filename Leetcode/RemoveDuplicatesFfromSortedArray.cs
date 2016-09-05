543c8a2392dae0fe952c5c46d6f519efc1bb3284
Max <max.iakovliev@gmail.com>
2016-09-06 00:01:40 +0800

    RemoveDuplicatesFfromSortedArray accepted solution

---
+++ Leetcode/RemoveDuplicatesFfromSortedArray.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.Leetcode
+{
+    public class RemoveDuplicatesFfromSortedArray
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            if (nums.Length < 2) return nums.Length;
+
+            var idx=1;
+            for (int i = 1; i < nums.Length; i++)
+                if (nums[i] != nums[i - 1])
+                    nums[idx++] = nums[i];
+
+            return idx;
+        }
+    }
+}
