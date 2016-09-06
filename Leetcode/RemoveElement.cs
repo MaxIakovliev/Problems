6a19727342c1415760a9b4c132ce50ae02ec7c51
Max <max.iakovliev@gmail.com>
2016-09-06 22:30:02 +0800

    RemoveElement accepted by leetcode

---
+++ Leetcode/RemoveElement.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.Leetcode
+{
+    /// <summary>
+    /// https://leetcode.com/problems/remove-element/
+    /// </summary>
+    public class RemoveElement
+    {
+        public int Solution1(int[] arr, int val)
+        {
+            if (arr == null || arr.Length == 0) return 0;
+            var count = arr.Length;
+            int start = 0;
+            int end = arr.Length - 1;
+            while (start <= end)
+            {
+                while (end > 0 && arr[end] == val)
+                {
+                    count--;
+                    end--;
+                }
+
+
+                if (arr[start] == val && start <= end)
+                {
+                    Swap(arr, start, end);
+                    count--;
+                    end--;
+                }
+                start++;
+            }
+            return count;
+        }
+        private void Swap(int[] arr, int start, int end)
+        {
+            int tmp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = tmp;
+        }
+    }
+}
---
+++ ProblemTest/Leetcode/RemoveElementTest.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+using Problems.Leetcode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemTest.Leetcode
+{
+    [TestFixture]
+    public class RemoveElementTest
+    {
+        /// <summary>
+        /// 1.input
+        /// 2.item to remove
+        /// 3.expected output len
+        /// 4 expected output
+        /// </summary>
+        private List<Tuple<int[], int, int, int[]>> _d;
+
+        [SetUp]
+        public void Init()
+        {
+            _d = new List<Tuple<int[], int, int, int[]>>();
+            _d.Add(new Tuple<int[], int, int, int[]>(null, 0, 0, null));
+            _d.Add(new Tuple<int[], int, int, int[]>(new int[] { 1 }, 2, 1, new int[] { 1 }));
+            _d.Add(new Tuple<int[], int, int, int[]>(new int[] { 1 }, 1, 0, new int[] { }));
+            _d.Add(new Tuple<int[], int, int, int[]>(new int[] { 1, 1 }, 1, 0, new int[] { }));
+            _d.Add(new Tuple<int[], int, int, int[]>(new int[] { 1, 1, 2 }, 1, 1, new int[] { 2 }));
+            _d.Add(new Tuple<int[], int, int, int[]>(new int[] { 1, 1, 2, 1 }, 1, 1, new int[] { 2 }));
+            _d.Add(new Tuple<int[], int, int, int[]>(new int[] { 1, 2, 1 }, 1, 1, new int[] { 2 }));
+            _d.Add(new Tuple<int[], int, int, int[]>(new int[] { 2, 1, 1 }, 1, 1, new int[] { 2 }));
+            _d.Add(new Tuple<int[], int, int, int[]>(new int[] { 2, 1, 1 }, 3, 3, new int[] { 2, 1, 1 }));
+        }
+
+        [Test]
+        public void TestRemoveElement()
+        {
+
+            var p = new RemoveElement();
+            int c = 0;
+            foreach (var item in _d)
+            {
+                var res = p.Solution1(item.Item1, item.Item2);
+                Console.WriteLine(c++);
+                Assert.AreEqual(item.Item3, res);
+
+            }
+        }
+    }
+}
