0044c090703667ae8c511e467d3df11f7c11a8ae
Max <max.iakovliev@gmail.com>
2016-09-07 00:06:41 +0800

    StrStrImplementation passed tests

---
+++ Leetcode/StrStrImplementation.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.Leetcode
+{
+    public class StrStrImplementation
+    {
+        /// <summary>
+        /// custom implementation   "IndexOf" function
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="q"></param>
+        /// <returns></returns>
+        public int StrStr(string s, string q)
+        {
+            if (  string.IsNullOrEmpty(q) ) return 0;
+            if (string.IsNullOrEmpty(s) || string.IsNullOrEmpty(q) || s.Length < q.Length) return -1;
+
+            int idx = 0;
+            int start = 0;
+            int end = 0;
+            while (idx+q.Length-1 < s.Length)
+            {
+                if (s[idx+q.Length-1] == q[q.Length-1])
+                {
+                    start = idx;
+                    end = idx + q.Length - 1;
+                    while (start <= end && end <= s.Length)
+                    {
+                        if (s[start] == q[start - idx] && s[end] == q[end - idx])
+                        {
+                            if (start == end) return idx;
+                            start++;
+                            end--;
+                            if (start > end) return idx;
+                        }
+                        else
+                        {
+                            idx = start;
+                            break;
+                        }
+                    }
+                }
+                else
+                    idx++;// =idx+ q.Length;
+            }
+            return -1;
+        }
+    }
+}
---
+++ ProblemTest/Leetcode/StrStrImplementationTest.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using Problems.Leetcode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemTest.Leetcode
+{
+    [TestFixture]
+    public class StrStrImplementationTest
+    {
+        private List<Tuple<string, int, string>> _d;
+        [SetUp]
+        public void Init()
+        {
+            _d = new List<Tuple<string, int, string>>();
+            _d.Add(new Tuple<string, int, string>("", 0, ""));
+            _d.Add(new Tuple<string, int, string>("abc", -1, "d"));
+            _d.Add(new Tuple<string, int, string>("abc", 0, "a"));
+            _d.Add(new Tuple<string, int, string>("aac", 0, "a"));
+            _d.Add(new Tuple<string, int, string>("abc", 1, "b"));
+            _d.Add(new Tuple<string, int, string>("abcd", 1, "bc"));
+            _d.Add(new Tuple<string, int, string>("aqqwbcd", 3, "wbc"));
+            _d.Add(new Tuple<string, int, string>("aqqwbqqccd", 5, "qqc"));
+            _d.Add(new Tuple<string, int, string>("mississippi", 4, "issip"));
+            _d.Add(new Tuple<string, int, string>("mississippi", 6, "sipp"));
+        }
+        
+        [Test]
+        public void TestStrStrFunction()
+        {
+            var p = new StrStrImplementation();
+            int count = 0;
+            foreach(var item in _d)
+            {
+                Console.WriteLine(count++);
+                var res = p.StrStr(item.Item1, item.Item3);
+                Assert.AreEqual(item.Item2, res);
+            }
+        }
+    }
+}
