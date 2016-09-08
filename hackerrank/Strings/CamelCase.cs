fb55795287bec716fbdb1c67b4f885b87c8982b1
Max <max.iakovliev@gmail.com>
2016-09-08 22:59:10 +0800

    Pangrams and CamelCase accepted by hakerrank

---
+++ hackerrank/Strings/CamelCase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.hackerrank.Strings
+{
+    public class CamelCase
+    {
+        public int Solution1(string s)
+        {
+            int count = 1;
+            for(int i=0; i<s.Length; i++)
+            {
+                if(char.IsUpper(s[i]))
+                    count++;
+            }
+            Console.WriteLine(count);
+            return count;
+        }
+    }
+}
---
+++ hackerrank/Strings/Pangrams.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.hackerrank.Strings
+{
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/pangrams
+    /// </summary>
+    public class Pangrams
+    {
+        public string Solution1(string s)
+        {
+            
+            s = s.ToLower();
+            var h = new HashSet<char>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (char.IsLetter(s[i]) && !h.Contains(s[i]))
+                {
+                    h.Add(s[i]);
+                }
+            }
+            if (h.Count == 26)
+                return "pangram";
+            else return "not pangram";
+        }
+    }
+}
