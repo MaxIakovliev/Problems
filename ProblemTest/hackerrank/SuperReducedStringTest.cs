43412fcab46e796dbaa56cd9f3374abbc8d2c434
Max <max.iakovliev@gmail.com>
2016-09-08 21:55:00 +0800

    SuperReducedString Accepted by hackerrank

---
+++ ProblemTest/hackerrank/SuperReducedStringTest.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using Problems.hackerrank.Strings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemTest.hackerrank
+{
+    [TestFixture]
+    public class SuperReducedStringTest
+    {
+        private List<Tuple<string, string>> _d;
+
+        [SetUp]
+        public void Init()
+        {
+            _d = new List<Tuple<string, string>>();
+
+            _d.Add(new Tuple<string, string>("aaabccddd", "abd"));
+            _d.Add(new Tuple<string, string>("baab", "Empty String"));
+            _d.Add(new Tuple<string, string>("aa", "Empty String"));
+            _d.Add(new Tuple<string, string>("aaabccddd", "abd"));
+            _d.Add(new Tuple<string, string>("zztqooauhujtmxnsbzpykwlvpfyqijvdhuhiroodmuxiobyvwwxupqwydkpeebxmfvxhgicuzdealkgxlfmjiucasokrdznmtlwh", "tqauhujtmxnsbzpykwlvpfyqijvdhuhirdmuxiobyvxupqwydkpbxmfvxhgicuzdealkgxlfmjiucasokrdznmtlwh"));
+        }
+
+        [Test]
+        public void SuperReducedStringSolution1Test()
+        {
+            var p = new SuperReducedString();
+            var count = 0;
+            foreach(var item in _d)
+            {
+                Console.WriteLine(count++);
+                var res = p.Solution1(item.Item1);
+                Assert.AreEqual(item.Item2, res);
+            }
+        }
+    }
+}
---
+++ hackerrank/Strings/SuperReducedString.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.hackerrank.Strings
+{
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/reduced-string
+    /// </summary>
+    public class SuperReducedString
+    {
+        public string Solution1(string s)
+        {
+
+            if (string.IsNullOrWhiteSpace(s)) return "Empty String";
+            if (s.Length == 1) return s;
+
+            var sb = new StringBuilder(s);
+            char? prev = sb[0];
+            int count = 1, idx = 1;
+
+            var isRemoved = false;
+            while (true)
+            {
+               
+                if (sb[idx] == prev)
+                {
+                    count++;
+                    idx++;
+                }
+                else
+                {
+                    prev = sb[idx];
+                    count = 1;
+                    idx++;
+                }
+
+                if (count == 2)
+                {
+                    sb.Remove(idx - 2, 2);
+                    if (sb.Length == 0) break;
+                    idx = 0;
+                    prev = sb[idx];
+                    idx++;
+                    isRemoved = true;
+                
+                }
+                if (idx > sb.Length - 1 && !isRemoved)
+                    break;
+
+                if (idx > sb.Length - 1)
+                {
+                    idx = 0;
+                    prev = sb[idx];
+                    idx++;
+                    count = 1;
+                    isRemoved = false;
+                }
+                
+            }
+            return sb.ToString() == "" ? "Empty String" : sb.ToString();
+        }
+    }
+}
