50bb1558f8e3e1de032597f6e192f3d4a88474b2
Max <max.iakovliev@gmail.com>
2016-09-09 20:46:24 +0800

    Beautiful Binary String accepted by hackerrank

---
+++ ProblemTest/hackerrank/BeautifulBinaryStringTest.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using Problems.hackerrank.Strings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemTest.hackerrank
+{
+    [TestFixture]
+    public class BeautifulBinaryStringTest
+    {
+        [Test]
+        public void BeautifulBinaryStringSolution1Test()
+        {
+            var d=new List<Tuple<string,int>>();
+            //d.Add(new Tuple<string, int>("0101010", 2));
+            //d.Add(new Tuple<string, int>("01100", 0));
+            //d.Add(new Tuple<string, int>("0100101010", 3));
+            //d.Add(new Tuple<string, int>("0100101010010", 4));
+            //d.Add(new Tuple<string, int>("0100010", 2));
+            //d.Add(new Tuple<string, int>("", 0));
+            //d.Add(new Tuple<string, int>("111111111111", 0));
+            //d.Add(new Tuple<string, int>("01010111111", 1));
+            d.Add(new Tuple<string, int>("01001001011101", 3));
+            var p = new BeautifulBinaryString();
+            foreach(var item in d)
+            {
+                Console.WriteLine(item.Item1);
+                int res = p.Solution1(item.Item1);
+                Assert.AreEqual(item.Item2, res);
+            }
+        }
+    }
+}
---
+++ hackerrank/Strings/BeautifulBinaryString.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.hackerrank.Strings
+{
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/beautiful-binary-string
+    /// </summary>
+    public  class BeautifulBinaryString
+    {
+        public int Solution1(string s)
+        {
+            StringBuilder sb = new StringBuilder(s);
+            int steps = 0;
+           int n=-1;
+            while((n=IndexOf(sb, "010"))>=0)
+            {
+                steps++;
+                sb[n + 2] = '1';
+            }
+            return steps;
+        }
+
+        public static int IndexOf(StringBuilder sb, string q)
+        {
+            if (string.IsNullOrEmpty(q)) return -1;
+            for (int i = 0; i <= sb.Length-q.Length+1; i++)
+            {
+                if (sb[i] == q[0])
+                {
+                    bool match = true;
+                    for (int j = i; j < i + q.Length; j++)
+                    {
+                        
+                        if (j>=sb.Length || sb[j] != q[j - i])
+                        {
+                            match = false;
+                            //i++;
+                            break;
+                        }
+                        
+                    }
+                    if (match)
+                        return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
