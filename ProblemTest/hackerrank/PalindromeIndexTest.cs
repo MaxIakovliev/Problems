757af91728e4e00e540c5c648e48df8d9bb8a00f
Max <max.iakovliev@gmail.com>
2016-09-11 22:56:25 +0800

    PalindromeIndex acceptet by hackerrank

---
+++ ProblemTest/hackerrank/PalindromeIndexTest.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using Problems.hackerrank.Strings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemTest.hackerrank
+{
+    [TestFixture]
+    public class PalindromeIndexTest
+    {
+        [Test]
+        public void PalindromeIndexSolution1Test()
+        {
+            var p = new PalindromeIndex();
+
+            var res=p.Solution1(new List<string>() { "hgygsvlfcwnswtuhmyaljkqlqjjqlqkjlaymhutwsnwcwflvsgygh" });
+            Assert.AreEqual(44, res[0]);
+
+
+        }
+    }
+}
---
+++ hackerrank/Strings/PalindromeIndex.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.hackerrank.Strings
+{
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/palindrome-index
+    /// </summary>
+    public class PalindromeIndex
+    {
+        public List<int> Solution1(List<string> s)
+        {
+            var result = new List<int>();
+            for (int i = 0; i < s.Count; i++)
+            {
+                string p = s[i];
+                int start = 0, end = p.Length - 1;
+                int res = -1;
+                while (start <= end)
+                {
+                    if (p[start] != p[end])
+                    {
+                        if(IsPaindrome(p,start+1,end))
+                        {
+                            res = start;
+                            result.Add(res);
+                            Console.WriteLine(res);
+                        }
+                        else
+                        {
+                            res = end;
+                            result.Add(res);
+                            Console.WriteLine(res);
+                        }
+                        break;
+                         
+                    }
+                    start++;
+                    end--;
+                }
+                if (res == -1)
+                {
+                    Console.WriteLine(res);
+                    result.Add(res);
+                
+                }
+
+            }
+            return result;
+        }
+
+
+        private bool IsPaindrome(string p, int start, int end)
+        {
+            while (start <= end)
+            {
+                if (p[start] != p[end])
+                {
+                    return false;
+                }
+                start++;
+                end--;
+            }
+            return true;
+        }
+
+    }
+}
