c842d0e4fc72167566c46b91da3369edd5dc94d2
Max <max.iakovliev@gmail.com>
2016-09-12 23:36:08 +0800

    GameofThronesI accepted

---
+++ hackerrank/Strings/GameofThronesI.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.hackerrank.Strings
+{
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/game-of-thrones
+    /// </summary>
+    class GameofThronesI
+    {
+        public void Solution1(string s)
+        {
+            var dict = new Dictionary<char, int>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (!dict.ContainsKey(s[i]))
+                    dict.Add(s[i], 1);
+                else
+                    dict[s[i]] = dict[s[i]] + 1;
+            }
+
+            bool isEven = s.Length % 2 > 0 ? true : false;
+            int countEven = 0;
+            foreach (var item in dict)
+            {
+                if (item.Value % 2 > 0)
+                    countEven++;
+            }
+            if (!isEven)
+            {
+                if (countEven ==0)
+                    Console.WriteLine("YES");
+                else
+                    Console.WriteLine("NO");
+            }
+            else
+            {
+                if (countEven ==1)
+                    Console.WriteLine("YES");
+                else
+                    Console.WriteLine("NO");
+            }
+
+        }
+    }
+}
